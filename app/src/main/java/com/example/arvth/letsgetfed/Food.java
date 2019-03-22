23a981e9ab308f1098151aed7554623884cd7f3e
mai <mppham52@gmail.com>
2019-03-22 13:28:20 -0400

    I added a few things to the Food class, but we won't be doing anything with the methods until after we figure out database things.

--- app/src/main/java/com/example/arvth/letsgetfed/Food.java
+++ app/src/main/java/com/example/arvth/letsgetfed/Food.java
@@ -1,7 +1,33 @@
 package com.example.arvth.letsgetfed;
 
-public class Food {
-    public Food() {
+import java.util.Date;
 
+/**
+ * This class models the behavior of a Food object.
+ */
+
+public class Food
+{
+    // Data
+    String name;
+    Date purchaseDate;
+    int minExpirationTime;
+    int maxExpirationTime;
+
+    // Constructor
+
+    /**
+     * Constructs a Food object with a given name,
+     */
+    public Food(String foodName, Date purchased, int minExpiration, int maxExpiration)
+    {
+        name = foodName;
+        purchaseDate = purchased;
+        minExpirationTime = minExpiration;
+        maxExpirationTime = maxExpiration;
     }
+
+    // Methods
+    // parse text to find food (match the name)
+    // we will do this after we decide what's going on with the database of foods
 }
