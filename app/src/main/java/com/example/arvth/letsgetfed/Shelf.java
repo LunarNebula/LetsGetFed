c22645969ba9a500215b47f489b4c73a55c23748
Anthony Vuolo <34285458+LunarNebula@users.noreply.github.com>
2019-03-22 13:42:08 -0400

    - Added Food and Shelf classes (no Food code) - Added java and XML files for opening screen and pantry - None of the above files are completed

--- app/src/main/java/com/example/arvth/letsgetfed/Shelf.java
+++ app/src/main/java/com/example/arvth/letsgetfed/Shelf.java
@@ -1,8 +1,12 @@
 package com.example.arvth.letsgetfed;
+import java.util.Arrays;
 
 public class Shelf {
     private Food[] food;
     private String label;
+    private int type;
+    public static final int DEFAULT = -1, FRIDGE = 0, FREEZER = 1, CUPBOARD = 2, COUNTER = 3;
+    private final String[] labels = {"default", "fridge", "freezer", "cupboard", "counter"};
     public Shelf(String label) {
         this.food = new Food[0];
         this.label = label;
@@ -13,5 +17,15 @@ public class Shelf {
     public String getLabel() {
         return this.label;
     }
-
+    public void setType(String type) {
+        this.type = Arrays.asList(this.labels).indexOf(type);
+    }
+    public void addFood(Food food) {
+        Food[] proxy_variable = new Food[this.food.length + 1];
+        for(int i = 0; i < this.food.length; i++) {
+            proxy_variable[i] = this.getFood(i);
+        }
+        proxy_variable[this.food.length] = food;
+        this.food = proxy_variable;
+    }
 }
