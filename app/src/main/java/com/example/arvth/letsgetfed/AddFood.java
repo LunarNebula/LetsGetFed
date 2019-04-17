ba729e26999a12957a948d65857f9b4225d8a587
nehaperumal <nehaperumal@gmaill.com>
2019-04-17 12:53:40 -0400

    addFood screen spinner is populated

--- app/src/main/java/com/example/arvth/letsgetfed/AddFood.java
+++ app/src/main/java/com/example/arvth/letsgetfed/AddFood.java
@@ -24,7 +24,7 @@ public class AddFood extends AppCompatActivity implements AdapterView.OnItemSele
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.foodtypes, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+// Specify the layout to use when the list of choices appearss
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
